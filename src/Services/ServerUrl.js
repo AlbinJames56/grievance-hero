f6bf2dba8aa94266118a2b0bc1b10ee131fb43f7
AlbinJames56 <albinjames56@gmail.com>
2024-11-21 01:31:14 +0530

    change server url to reder link /

--- src/Services/ServerUrl.js
+++ src/Services/ServerUrl.js
@@ -1,2 +1,2 @@
-export const SERVER_URL='https://grievanceserver-jvyh.onrender.com'
+export const SERVER_URL='https://grievanceserver-jvyh.onrender.com/'
 // export const SERVER_URL='http://localhost:3000/'
